db873cf90d7960b15343010d96620907e95de99c
EllaKaye <ella.kaye@gmail.com>
2023-10-23 22:28:17 +0100

    setup for creating posts

---
+++ R/post-setup.R
@@ -0,0 +1,95 @@
+# get current year
+current_year <- function() {
+	as.character(format(Sys.Date(), "%Y"))
+}
+
+# get the input url given day and year
+advent_input_url <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	paste0("https://adventofcode.com/", year, "/day/", day, "/input")
+}
+
+# get the url given day and year
+advent_url <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	paste0("https://adventofcode.com/", year, "/day/", day)
+}
+
+# get and save the input for a given day and year
+# adapted from https://github.com/dgrtwo/adventdrob/blob/main/R/input.R
+get_advent_input <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	
+	session <- Sys.getenv("ADVENT_SESSION")
+	if (session == "") {
+		stop("Must set ADVENT_SESSION in .Renviron")
+	}
+	
+	url <- advent_input_url(day, year)
+	
+	year_path <- here::here("input", year)
+	path <- paste0(year_path, "/", "day", day, ".txt")
+	
+	# check if there's a directory for the year in data and create one if not
+	if (!dir.exists(year_path)) {
+		dir.create(year_path)
+	}
+	
+	req <- httr::GET(url,
+									 httr::set_cookies(session = session),
+									 httr::write_disk(path, overwrite = TRUE))
+}
+
+# copy post-template to create a new post for a given day and year
+new_advent_post <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	
+	from_post <- here::here("post-template")
+	to_post <- here::here(year, paste0("day", day))
+	
+	dir.create(to_post)
+	file.copy(list.files(from_post, full.names = TRUE),
+						to_post,
+						recursive = TRUE)
+	
+	# get index.qmd from the new post
+	index <- readLines(paste0(to_post, "/index.qmd"))
+	
+	# replace title and YYYY and DD placeholders
+	index_with_year <- gsub("YYYY", year, index)
+	index_with_day <- gsub("DD", day, index_with_year)
+	
+	# evaluate reading in the input
+	index_with_input <- gsub("eval: false", "eval: true", index_with_day)
+	
+	# write the updated post
+	writeLines(index_with_input, paste0(to_post, "/index.qmd"))
+	
+}
+
+delete_advent_post <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	
+	post <- here::here(year, paste0("day", day))
+	unlink(post, recursive = TRUE)
+}
+
+# new_day function gets input and creates a new post
+new_day <- function(day, year = NULL) {
+	if (is.null(year)) {
+		year <- current_year()
+	}
+	
+	get_advent_input(day, year)
+	new_advent_post(day, year)
+}
