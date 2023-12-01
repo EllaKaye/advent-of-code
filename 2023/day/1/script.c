463c60d77411b539ad98939344938f70559dce1e
EllaKaye <ella.kaye@gmail.com>
2023-12-01 16:48:05 +0000

    files for C solution to Day 1 2023

--- 2023/day/1/script.c
+++ 2023/day/1/script.c
@@ -5,21 +5,31 @@
 
 int get_value(char input[]);
 
-FILE *fptr;
-
-// Open a file in read mode
-fptr = fopen("example-input", "r");
-
-// Store the content of the file
-char my_input[4];
-
 int main(void) {
-
-	char input[] = "seven3find";
 	
-	int value = get_value(input);
+	FILE *fptr;
 	
-	printf("%d\n", value);
+	// Open a file in read mode
+	fptr = fopen("input", "r");
+	
+	// Store the content of the file
+	char input_line[50];
+	
+	int total = 0;
+	
+	// Read the content and store it inside input_line
+	while (fgets(input_line, 50, fptr)) {
+		int value = get_value(input_line);
+		//printf("%d\n", value);
+		total += value;
+		//printf("%d\n", total);
+		
+	}
+	
+	// Close the file
+	fclose(fptr);
+	
+	printf("%d\n", total);
 }
 
 int get_value(char input[]) {
@@ -30,7 +40,7 @@ int get_value(char input[]) {
 	
 	for (int i = 0; i < length; i++) {
 		// find the first digit
-		if (input[i] >= '0' && input[i] <= '9') {
+		if (input[i] > '0' && input[i] <= '9') {
 			//printf("%c\n", input[i]);
 			// convert to int and update value
 			value = (input[i] - '0')*10;
@@ -40,8 +50,8 @@ int get_value(char input[]) {
 	}
 	
 	// now find the last digit
-	for (int i = length; i > 0; i--) {
-		if (input[i] >= '0' && input[i] <= '9') {
+	for (int i = length; i >= 0; i--) {
+		if (input[i] > '0' && input[i] <= '9') {
 
 			value += (input[i] - '0');
 			break;
