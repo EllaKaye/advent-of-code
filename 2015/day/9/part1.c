c25d19c382df1611384aab3d04838f19778be239
EllaKaye <ella.kaye@gmail.com>
2025-02-24 10:52:46 +0000

    2015-9

--- 2015/day/9/part1.c
+++ 2015/day/9/part1.c
@@ -1,12 +1,19 @@
 // template that's likely to be close to what's required for an AoC puzzle
 #include <stdio.h>
 #include <string.h>
-#define LINE_LENGTH 50 //including '\0'
 
 // default input file
 #define INPUT_FILE "input"
 
-int process_line(const char line[]);
+// constants
+#define N_LOCATIONS 8
+char *locations[N_LOCATIONS] = {"AlphaCentauri", "Arbre", "Faerun", "Norrath", "Snowdin", "Straylight", "Tambi", "Tristram"};
+int dist_mat[N_LOCATIONS][N_LOCATIONS];
+
+// function prototypes
+unsigned int location_i(char *loc);
+int shortest_path_from_location(char *loc);
+void print_dist_mat(void);
 
 int main(int argc, char *argv[]) {
 	
@@ -32,18 +39,27 @@ int main(int argc, char *argv[]) {
 	// OR WHATEVER ELSE IS REQUIRED FOR THE PUZZLE
 	int total = 0;
 	
-	// Read the content and store it inside input_line buffer
-	// ALTERNATIVES: 
-		// read in chars individually with fgetc
-		// read in formatted string with fscanf
-	char input_line[LINE_LENGTH];
-	while (fgets(input_line, LINE_LENGTH, fptr)) 
+	// Read and save the file content 
+	char from[14];
+	char to[14];
+	int dist;
+	
+	// fill dist_mat
+	/// fill doagonals with zero
+	for (int i = 0; i < N_LOCATIONS; i++)
 	{
-		int value = process_line(input_line);
-		
-		// WHATEVER WE NEED HERE
-		total += value;
+		dist_mat[i][i] = 0;
 	}
+	
+	/// fill off-diagonals from file
+	while (fscanf(fptr, "%s to %s = %d", from, to, &dist) == 3) 
+	{
+		//printf("from %d to %d: %d\n", location_i(from), location_i(to), dist);
+		dist_mat[location_i(from)][location_i(to)] = dist;
+		dist_mat[location_i(to)][location_i(from)] = dist;
+	}
+	
+	print_dist_mat();
 	
 	// close the file
 	fclose(fptr);
@@ -52,12 +68,36 @@ int main(int argc, char *argv[]) {
 	printf("%d\n", total);
 }
 
-int process_line(const char line[]) {
+unsigned int location_i(char *loc)
+{
+	for (unsigned int i = 0; i < N_LOCATIONS; i++)
+	{
+		if (strcmp(loc, locations[i]) == 0) {
+			return(i);
+		}
+	}
 	
-	int value = 0;
-	
-	// PROCESS THE LINE!
-	printf("%s\n", line);
-	
-	return value;
+	// if haven't found the location
+	printf("Not a valid location.\n");
+	return N_LOCATIONS;
 }
+
+int shortest_path_from_location(char *loc) 
+{
+	loc_i <- location_i(loc);
+	
+	// write a not_visited(int loc) function!
+}
+
+void print_dist_mat(void)
+{
+	for (int i = 0; i < N_LOCATIONS; i++)
+	{
+		for (int j = 0; j < N_LOCATIONS; j++)
+		{
+			printf("%3d ", dist_mat[i][j]);
+		}
+	printf("\n");
+	}
+}
+
